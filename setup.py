9f8a008761df84570888941e0bf4e1314812bfcc
Justin Van Winkle <justin@milo.com>
2014-01-16 11:56:30 -0800

    clean up setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,18 @@
 from distutils.core import setup, Extension
 from glob import glob
 
+VERSION = '0.9'
 
 if __name__ == '__main__':
     src_files = glob('src/*.c')
     extensions = [Extension("suffix_array",
                             src_files,
                             language='c')]
+    description = "Fast Suffix Array for Python"
     setup(name='suffix_array',
+          version=VERSION,
+          author='Justin Van Winkle',
+          author_email='justin.vanwinkle@gmail.com',
+          url='https://github.com/justinvanwinkle/suffix_array',
+          description=description,
           ext_modules=extensions)
